32843b8df5f3905a28b8cdd482cfab45123284ad
BorjaEst <boressan@outlook.com>
2020-12-11 18:18:13 +0100

    Add tests inside module as framework suggests

--- cicd/module_template.py
+++ cicd/module_template.py
@@ -1,9 +1,34 @@
 """Python module template."""
+import unittest
 
 
-def hello_world(*arg, **kvarg):
+def hello_world(*arg, **kwarg):
     print("Hello World")
-    if arg or kvarg:
+    if arg or kwarg:
         print(f"  - Arguments as Inputs: {arg}")
-        print(f"  - Arguments as KeyVal: {kvarg}")
+        print(f"  - Arguments as KeyVal: {kwarg}")
     return True
+
+
+class Tests(unittest.TestCase):
+    """Tests for `module_template` package."""
+    def __init__(self, testName):
+        super().__init__(testName)
+
+    def setUp(self):
+        """Set up test fixtures, if any (once per case).
+        """
+        self.value = 1
+
+    def tearDown(self):
+        """Tear down test fixtures, if any (once per case).
+        """
+        self.value = 0
+
+    def test_001(self):
+        self.assertTrue(hello_world())
+        self.assertFalse(False)
+
+    def test_002(self):
+        self.assertEqual(self.value, 1)
+        self.assertIsInstance(self.value, int)

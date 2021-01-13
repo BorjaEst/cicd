18e1fe80f9fd27108ac9eddb74500fc0e1ad46eb
BorjaEst <boressan@outlook.com>
2021-01-13 18:20:11 +0100

    Implementation of unittest groups

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,16 +5,42 @@ import unittest
 from cicd import module_template
 
 
-def group(*tests):
-    suite = unittest.TestSuite()
-    for testCase in tests:
-        suite.addTest(testCase)
-    return suite
+def init(Level):
+    print(">>--Init {}".format(Level))
 
 
-def load_tests(loader, tests, pattern):
-    group1 = group(
-        module_template.Tests('test_001'),
-        module_template.Tests('test_002', value=1)
-    )
-    return group1
+def down(Level):
+    print("<<--Down {}".format(Level))
+
+
+class Tests(unittest.TestCase):
+    """Tests for application."""
+
+    def group_001(self):
+        """Run tests calling setUp/tearDown from module"""
+        init(Level="group_001")
+        module_template.Tests('test_001').run()
+        module_template.Tests('test_002', value=1).run()
+        down(Level="group_001")
+
+    def group_002(self):
+        """Run tests omiting setUp/tearDown from module"""
+        init(Level="group_002")
+        module_template.Tests('test_001').test_001()
+        module_template.Tests('test_002', value=2).test_002()
+        down(Level="group_002")
+
+    def group_010(self, Level):
+        """New group that takes the previous 2"""
+        init(Level=Level)
+        self.group_001()
+        self.group_002()
+        down(Level=Level)
+
+    def test_001(self):
+        """Test that calls group1 using level 001"""
+        self.group_010(Level="Root test_001")
+
+    def test_002(self):
+        """Test that calls group1 using level 002"""
+        self.group_010(Level="Root test_002")

d7bb815e7b4ef1dc51aeea1a04f40301a639a2c9
BorjaEst <boressan@outlook.com>
2021-01-15 11:34:16 +0100

    Suite can go out of TestCase and be shared

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,6 +5,14 @@ import unittest
 from cicd import module_template
 
 
+def suite_001(param):
+    """Returns suite_001 suite"""
+    suite = unittest.TestSuite()
+    suite.addTest(module_template.Tests('test_002', value=param+1))
+    suite.addTest(module_template.Tests('test_002', value=param-1))
+    return suite
+
+
 class Group_001(unittest.TestCase):
     """Tests for application, Group 001"""
 
@@ -16,23 +24,9 @@ class Group_001(unittest.TestCase):
         """Tear down group fixtures, if any (once per case)."""
         print("Group 001 tearDown")
 
-    def suite_001(self):
-        """Returns suite_001 suite"""
-        suite = unittest.TestSuite()
-        suite.addTest(module_template.Tests('test_001'))
-        suite.addTests(self.suite_002())
-        return suite
-
-    def suite_002(self):
-        """Returns suite_002 suite"""
-        suite = unittest.TestSuite()
-        suite.addTest(module_template.Tests('test_002', value=1))
-        suite.addTest(module_template.Tests('test_002', value=2))
-        return suite
-
     def test_001(self):
         runner = unittest.TextTestRunner()
-        result = runner.run(self.suite_001())
+        result = runner.run(suite_001(param=1))
         self.assertTrue(result.wasSuccessful())
 
 
@@ -47,13 +41,7 @@ class Group_002(unittest.TestCase):
         """Tear down group fixtures, if any (once per case)."""
         print("Group 002 tearDown")
 
-    def suite_001(self):
-        """Returns suite_001 suite"""
-        suite = unittest.TestSuite()
-        suite.addTest(Group_001('test_001'))
-        return suite
-
     def test_001(self):
         runner = unittest.TextTestRunner()
-        result = runner.run(self.suite_001())
+        result = runner.run(suite_001(param=-1))
         self.assertTrue(result.wasSuccessful())
